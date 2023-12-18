f765cc4fff0302a3cb8c9e083bb63f4a01fd47b4
mansinakrani <mansinakrani006@gmail.com>
2023-12-18 12:30:59 +0530

    initial commit

---
+++ src/components/__tests__/example.test.js
@@ -0,0 +1,10 @@
+//src/components/__tests/example.test.js
+import { test, expect } from "vitest";
+
+function sum(a, b) {
+  return a + b;
+}
+
+test("add 2 numbers", () => {
+  expect(sum(2,3)).toEqual(5);
+});
